f961130140704367a2e652b29d41c3947dc0aa15
Arman_Samma <arman.samma3@gmail.com>
2019-08-03 15:43:12 +0100

    added GenderResponse class

--- build/lib/namsorclient/__init__.py
+++ build/lib/namsorclient/__init__.py
@@ -15,3 +15,36 @@ class NamsorClient:
     def api_get(self, url="/api2/json/gender/Lelouch/Lamperouge"):
         return requests.get(url='https://v2.namsor.com/NamSorAPIv2/api2/json/genderBatch',
                             headers={"X-API-KEY": self.api_key})
+
+
+
+class GenderResponse:
+    ID = ""
+    firstName = ""
+    lastName = ""
+    likelyGender = ""
+    genderScale = ""
+    score = 0.0
+    probabilityCalibrated = 0.0
+
+# class originResponse:
+#     id = ""
+#     firstName = ""
+#     lastName = ""
+#     countryOrigin = ""
+#     countryOriginAlt = ""
+#     score = ""
+#     regionOrigin = ""
+#     topRegionOrigin = ""
+#     subRegionOrigin = ""
+
+# class RaceEthnicityResponse:
+#     id = ""
+#     firstName = ""
+#     lastName = ""
+#     raceEthnicityAlt =  "W_NL"
+#     raceEthnicity = "W_NL"
+#     score = 0
+
+# class DiasporaResponse:
+    
