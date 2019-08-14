4f544590891f0be934d5806b2f75b38b21bd8c17
Joseph Pallipadan <31515243+TheStarvedGlutton@users.noreply.github.com>
2019-08-14 13:30:20 -0400

    Added all personal GET endpoints and wrapper classes"

--- namsorclient/__init__.py
+++ namsorclient/__init__.py
@@ -2,6 +2,10 @@ import requests
 
 from country_codes import CountryCodes
 from models import GenderResponse
+from models import OriginResponse
+from models import DiasporaResponse
+from models import ParseNameResponse
+from models import RaceEthnicityResponse
 from request_objects import *
 import helpers
 import json
@@ -43,8 +47,7 @@ class NamsorClient:
 
         Args:
 
-            url (str): ending portion of NamsorAPI url to desired section 
-
+            url (str): ending portion of NamsorAPI url to desired section
             data (dict): the high throughput data to process
 
         Returns:
@@ -151,15 +154,112 @@ class NamsorClient:
         url = f"genderFullGeo/{full_name}/{country_code.value}"
         return GenderResponse(self.__api_get(url=url).json())
 
-        def genderFull(self, full_name: str) -> GenderResponse:
-            """Infer the likely gender of a full name, ex. John H. Smith
+    def genderFull(self, full_name: str) -> GenderResponse:
+        """Infer the likely gender of a full name, ex. John H. Smith
 
-            Args:
-                full_name (str): The name to be classified.
+        Args:
+            full_name (str): The name to be classified.
 
-            Returns:
-                GenderResponse: An Object which is a wrapper of the API's response object for this particular endpoint.
-            """
+        Returns:
+            GenderResponse: An Object which is a wrapper of the API's response object for this particular endpoint.
+        """
 
-        url = f"genderFull/{full_name}"
-        return GenderResponse(self.__api_get(url=url).json())
+    url = f"genderFull/{full_name}"
+    return GenderResponse(self.__api_get(url=url).json())
+
+    def usRaceEthnicity(self, first_name: str, last_name: str) -> RaceEthnicityResponse:
+        """Infer a US resident's likely race/ethnicity according to US Census taxonomy W_NL (white, non latino), HL (hispano latino),  A (asian, non latino), B_NL (black, non latino).
+
+        Args:
+            first_name (str): The desired first name.
+            last_name (str): The desired last name.
+
+        Returns:
+            OriginResponse: An object which is a wrapper of the API's response object for this particular endpoint.
+        """
+        url = f"usRaceEthnicity/{first_name}/{last_name}"
+        return RaceEthnicityResponse(self.__api_get(url=url).json())
+
+    def usRaceEthnicityZIP5(self, first_name: str, last_name: str, zip5_code: str) -> RaceEthnicityResponse:
+        """Infer a US resident's likely race/ethnicity according to US Census taxonomy, using ZIP5 code info. Output is W_NL (white, non latino), HL (hispano latino),  A (asian, non latino), B_NL (black, non latino).
+
+        Args:
+            first_name (str): The desired first name
+            last_name (str): The desired last name
+            zip5_code (str): The zip code to aid with classification
+
+        Returns:
+            OriginResponse: An object which is a wrapper of the API's response object for this particular endpoint.
+        """
+
+        url = f"usRaceEthnicity/{first_name}/{last_name}/{zip5_code}"
+        return RaceEthnicityResponse(self.__api_get(url=url).json())
+
+    def diaspora(self, country_code: CountryCodes, first_name: str, last_name: str) -> DiasporaResponse:
+        """Infer the likely ethnicity/diaspora of a personal name, given a country of residence ISO2 code
+
+        Args:
+            country_code (CountryCodes): The country code to aid with classification.
+            first_name (str): The desired first name.
+            last_name (str): The desired last name
+
+        Returns:
+            OriginResponse: An object which is a wrapper of the API's response object for this particular endpoint.
+        """
+
+        url = f"diaspora/{country_code.value}/{first_name}/{last_name}"
+        return DiasporaResponse(self.__api_get(url=url).json())
+
+    def parseNameGeo(self, full_name: str, coutry_code: CountryCodes) -> ParseNameResponse:
+        """Infer the likely first/last name structure of a name, ex. John Smith or SMITH, John or SMITH; John, given an ISO2 country of residence.
+
+        Args:
+            full_name (str): The full name to be parsed.
+            coutry_code (CountryCodes): The country code to aid with parsing
+
+        Returns:
+            ParseNameResponse: An object which is a wrapper of the API's response object for this particular endpoint.
+        """
+
+        url = f"parseName/{full_name}/{country_code.value}"
+        return ParseNameResponse(self.__api_get(url=url).json())
+
+    def origin(self, first_name: str, last_name) -> OriginResponse:
+        """Infer the likely country of origin of a personal name. Assumes names as they are in the country of origin. For US, CA, AU, NZ and other melting-pots : use 'diaspora' instead.
+
+        Args:
+            first_name (str): The desired first name.
+            last_name ([type]): The desired last name
+
+        Returns:
+            OriginResponse: An object which is a wrapper of the API's response object for this particular endpoint.
+        """
+
+        url = f"origin/{first_name}/{last_name}"
+        return OriginResponse(self.__api_get(url=url).json())
+
+    def country(self, full_name: str) -> OriginResponse:
+        """Infer the likely country of residence of a personal full name, or one surname. Assumes names as they are in the country of residence OR the country of origin.
+
+        Args:
+            full_name (str): The name whose country of residence should be determined.
+
+        Returns:
+            OriginResponse: An object which is a wrapper of the API's response object for this particular endpoint.
+        """
+
+        url = f"country/{first_name}/{last_name}"
+        return OriginResponse(self.__api_get(url=url).json())
+
+    def parseName(self, full_name: str) -> ParseNameResponse:
+        """Infer the likely first/last name structure of a name, ex. John Smith or SMITH, John or SMITH; John.
+
+        Args:
+            full_name (str): The name to be parsed
+
+        Returns:
+            ParseNameResponse: An object which is a wrapper of the API's response object for this particular endpoint.
+        """
+
+        url = f"parseName/{full_name}"
+        return ParseNameResponse(self.__api_get(url=url).json())
--- namsorclient/models.py
+++ namsorclient/models.py
@@ -1,7 +1,8 @@
 import requests
- 
- 
-class GenderResponse: 
+import re
+
+
+class GenderResponse:
     """
         A class that represents a response that contains gender information
 
@@ -14,9 +15,16 @@ class GenderResponse:
             score (float): the score of the precision of the gender information provided ranging from 0 to 50
             probability_calibrated (float): the probability that the gender information provided is correct
     """
+
+    ID = ""
+    first_name = ""
+    last_name = ""
+    likely_gender = ""
+    gender_scale = ""
+    score = 0.0
+
     def __init__(self, api_response: dict):
         """ Constructor
-        
         Args:
             api_response (dict): the json format (dict) of the NamsorAPI response received from a GET/POST request
         """
@@ -27,12 +35,23 @@ class GenderResponse:
         self.likely_gender = api_response['likelyGender']
         self.gender_scale = int(api_response['genderScale'])
         self.score = float(api_response['score'])
-        self.probability_calibrated = float(api_response['probabilityCalibrated'])
+        self.probability_calibrated = float(
+            api_response['probabilityCalibrated'])
         # Initializing the GenderResponse attributes with values of corresponding attribute names or keys of the API responsein json format
 
 
 class OriginResponse:
 
+    ID = ""
+    first_name = ""
+    last_name = ""
+    likely_gender = ""
+    score = 0.0
+    country_origin = ""
+    country_origin_alt = ""
+    region_origin = ""
+    top_region_origin = ""
+    sub_region_origin = ""
 
     def __init__(self, api_response: dict):
         self.ID = api_response['id']
@@ -45,13 +64,94 @@ class OriginResponse:
         self.top_region_origin = api_response['top_region_origin']
         self.sub_region_origin = api_response['sub_region_origin']
 
-# class RaceEthnicityResponse:
-#     ID = ""
-#     firstName = ""
-#     lastName = ""
-#     raceEthnicityAlt =  "W_NL"
-#     raceEthnicity = "W_NL"
-#     score = 0
 
-# class DiasporaResponse:
+class RaceEthnicityResponse:
+    ID = ""
+    first_name = ""
+    last_name = ""
+    race_ethnicity_alt = "W_NL"
+    race_ethnicity = "W_NL"
+    score = 0
 
+    def __init__(self, api_response: dict):
+        self.ID = api_response['id']
+        self.first_name = api_response['firstName']
+        self.last_name = api_response['lastName']
+        self.race_ethnicity = api_response['raceEthnicity']
+        self.race_ethnicity_alt = api_response['raceEthnicityAlt']
+        self.score = api_response['score']
+
+
+class DiasporaResponse:
+
+    ID = ""
+    fist_name = ""
+    last_name = ""
+    score = 0.0
+    ethnicityAlt = ""
+    ethnicity = ""
+    lifted = ""
+    countryIso2 = ""
+
+    def __init__(self, api_response: dict):
+        self.ID = api_response['id']
+        self.first_name = api_response['firstName']
+        self.last_name = api_response['lastName']
+        self.score = float(api_response['score'])
+        self.ethnicityAlt = api_response['ethnicityAlt']
+        self.ethnicity = api_response['ethnicity']
+        self.lifted = api_response['lifted']
+        self.countryIso2 = api_response['countryIso2']
+
+
+class ParseNameResponse:
+
+    ID = ""
+    name = ""
+
+    name_parser_type = ""
+    name_parser_type_alt = ""
+    first_last_name = ""
+
+    score = ""
+
+    def __init__(self, api_response: dict):
+        self.ID = api_response['id']
+        self.name = api_response['name']
+        self.name_parser_type = NameParserTypeWrapper(
+            api_response['nameParserType'])
+        self.name_parser_type_alt = NameParserTypeWrapper(
+            api_response['nameParserTypeAlt'])
+        self.first_last_name = api_response['firstLastName']
+        self.score = api_response['score']
+
+
+class NameParserTypeWrapper:
+
+    _raw_string = ""
+    first_name_count = 0
+    last_name_count = 0
+
+    def __init__(self, raw_string: str):
+        self.raw_string = raw_string
+        regex = re.fullmatch(r"FN([0-9]+)LN([0-9]+)", raw_string)
+        self.first_name_count = regex.groups()[0]
+        self.last_name_count = regex.groups()[1]
+
+    def __repr__(self):
+        return self._raw_string
+
+
+class firstLastNameWrapper:
+
+    ID = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, raw_dict: dict):
+        self.ID = raw_dict['id']
+        self.first_name = raw_dict['firstName']
+        self.last_name = raw_dict['lastName']
+
+    def __repr__(self):
+        return f'First Name: {self.first_name} | Last Name: {self.last_name}'
